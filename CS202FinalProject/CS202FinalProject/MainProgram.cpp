c763ce9118e3fa965d83d8ddef3df661717941d1
unknown <nguyen31hoang08minh2003@gmail.com>
2022-10-29 07:58:59 +0700

    Upload Main Program class

---
+++ CS202FinalProject/CS202FinalProject/MainProgram.cpp
@@ -0,0 +1,15 @@
+#include "MainProgram.h"
+
+MainProgram::MainProgram(): videoMode(SCREEN_WIDTH, SCREEN_HEIGHT) {
+	(this -> window) = new sf::RenderWindow(videoMode, "Road crossing game", sf::Style::Titlebar | sf::Style::Close);
+};
+
+MainProgram::~MainProgram() {
+	delete (this -> window);
+};
+
+void MainProgram::run() {
+	while ((this -> window) ->isOpen()) {
+
+	}
+}
---
+++ CS202FinalProject/CS202FinalProject/MainProgram.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+class MainProgram {
+private:
+
+	const int SCREEN_WIDTH = 1500, SCREEN_HEIGHT = 1000;
+
+	sf::RenderWindow* window;
+	sf::VideoMode videoMode;
+
+public:
+	MainProgram();
+	~MainProgram();
+
+	void run();
+};
