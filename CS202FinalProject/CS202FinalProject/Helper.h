d5185c057ff6961b0ec7f11066a6af17c0128d7e
unknown <nguyen31hoang08minh2003@gmail.com>
2022-11-14 13:05:00 +0700

    Update Helper class

--- CS202FinalProject/CS202FinalProject/Helper.h
+++ CS202FinalProject/CS202FinalProject/Helper.h
@@ -2,6 +2,8 @@
 
 #include <random>
 #include <string>
+#include <vector>
+#include <cstring>
 
 class Helper {
 private:
@@ -10,5 +12,14 @@ public:
 	static long long getRandomInteger(const long long l, const long long r);
 	static std::string convertIntToString(const int number);
 	static int convertStringToInt(const std::string &s);
+	/*
+	static std::vector<std::string> splitString(const std::string& text, const std::string& seperate);
+	static std::vector<std::string> splitString(const std::string& text, const std::string& seperate, int maximum);
+	static std::string replaceString(const std::string& text, const std::string& oldSubstring, const std::string& newSubstring);
+	static std::string replaceString(const std::string& text, const std::string& oldSubstring, const std::string& newSubstring, int maximum);
+	*/
+	static int getRandomIntegerFromList(const std::vector<int>& numbers);
+	static std::string getLowercaseString(std::string s);
+	static bool readInteger(const std::string& s, int& i, int& result);
 
 };
